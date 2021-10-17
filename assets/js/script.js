0837b3504ab198630993b05e25dd0fb6f63508d7
Eric <ericyoung111@gmail.com>
2021-10-17 09:11:20 -0600

    forgot to commit initial files. This commit contains code to put the current time a the top of the app, populates rows for each hour of the working day, rows are text boxes with a button that sends to local storage, rows change colors based on past due, currently due or due in future.

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+
+var appendLocalStorage = function() {
+    $(".append-item").click(function() {
+        var text = $(this).siblings(".col-10").val()
+        var id = $(this).siblings(".col-10").attr("id")
+        localStorage.setItem(id, text);
+
+    })
+}
+
+var populateFromStorage = function() {
+    for(i=6; i<18; i++) {
+        //var stringI = toString(i)
+        console.log(localStorage.getItem('i'))
+        if(localStorage.getItem('i')){
+            console.log("for if")
+            console.log(localStorage.getItem('i'))
+        }
+    }
+}
+
+//for loop that iterates through time blocks to color them based on relation to current hour of day
+for(i=6; i<18; i++) {
+    //add class for before
+    if (i<moment().format("H")) {
+        $("#"+i).addClass("bg-danger")
+    }
+
+    //add class for equal to
+    else if (moment().format("H")==i) {
+        $("#"+i).addClass("bg-warning")
+    } 
+
+    //add class for after
+    else {
+        $("#"+i).addClass("bg-success")
+    } 
+}
+
+appendLocalStorage();
+populateFromStorage();
